a108b67fbee05b908b8462da8f17cfb1060d5860
Wilsley Germano <wilsleygermano@gmail.com>
2024-04-17 09:26:15 -0300

    Initial Commit

---
+++ TomatoSauceTimer/TomatoSauceTimerApp.swift
@@ -0,0 +1,32 @@
+//
+//  TomatoSauceTimerApp.swift
+//  TomatoSauceTimer
+//
+//  Created by Wilsley Germano on 17/04/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct TomatoSauceTimerApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
